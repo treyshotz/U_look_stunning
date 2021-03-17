f94daf856f525387e0fb0de0dc667a25dd4664ff
Simon Jensen <simon.jensen.lg@gmail.com>
2021-03-17 11:29:15 +0100

    Added folders for views and js/css. Small improvements on signal server

--- webrtc/server.js
+++ webrtc/server.js
@@ -1,30 +1,47 @@
-var express = require('express');
-var app = express();
-var {"v4": uuidv4} = require('uuid');
-var http = require('http');
+var os = require('os')
+var express = require('express')
+var app = express()
+var http = require('http')
+var server = http.createServer(app)
+server.listen(3001)
 
-var httpServer = http.createServer(app);
+//For signaling
+var socketIo = require('socket.io')
+var io = socketIo(server)
 
-httpServer.listen(3001);
+//For generating roomIds
+var {"v4": uuidv4} = require('uuid')
 
-
-//For https
-//var https = require('https');
-//var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
-//var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
-//var credentials = {key: privateKey, cert: certificate};
-//var httpsServer = https.createServer(credentials, app);
-//httpsServer.listen(8443);
-
-
-app.set('view', 'ejs')
+//Folder for js and css
 app.use(express.static('public'))
 
-//If user doesn't enter any room id a random one is created and passed
+//If user doesn't enter any room id a random one is created
+//Redirects client to the created roomid
 app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`)
 })
 
-app.get('/room', (req, res) => {
-    res.render('room', { roomId: req.params.room})
+app.get('/:room', (req, res) => {
+    res.render('index.ejs', { roomId: req.params.room})
+})
+
+io.sockets.on('connection', (client) => {
+    console.log('Client connected: ' + client.id)
+
+    client.on('offer', (details) => {
+        client.broadcast('offer', details)
+        console.log('Client: ' + client.id + ' offers ' + JSON.stringify(details))
+    })
+    
+    client.on('answer', (details) => {
+        client.broadcast('answer', details)
+        console.log('Client: ' + client.id + ' responds with ' + JSON.stringify(details))
+    })
+
+    client.on('candidate', (details) => {
+        client.broadcast('candidate', details)
+        console.log('Candidate: ' + JSON.stringify(details))
+    })
+
+    client.broadcast.emit('createoffer', {})
 })
