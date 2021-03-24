e0641594055fe2a8570fac724d28e85b717f7f79
Simon Jensen <simon.jensen.lg@gmail.com>
2021-03-24 18:39:28 +0100

    Fixed z indexing on label when fullscreen video

--- webrtc/public/main.js
+++ webrtc/public/main.js
@@ -42,7 +42,7 @@ function start(){
         port: '9000',
         config: {
             'iceServers': [
-                { url: 'stun:stun1.l.google.com:19302' },
+                { url: 'stun:158.38.166.242:4040' },
             ]
         }
     });
@@ -61,16 +61,16 @@ function start(){
         navigator.mediaDevices.getUserMedia(constraints)
         .then(stream => {
             myStream = stream;
-            var myVideo = document.createElement('video')
+            var myVideo = document.createElement('video');
             myVideo.muted = true;
-            addNewStream(myVideo, myStream, id, myUsername)
-            socket.emit('join', ROOM, id, myUsername)
+            addNewStream(myVideo, myStream, id, myUsername);
+            socket.emit('join', ROOM, id, myUsername);
         })
         .catch(err => {
-            console.log(err)
-            myStream = new MediaStream()
-            socket.emit('join', ROOM, id, myUsername)
-        })
+            console.log(err);
+            myStream = new MediaStream();
+            socket.emit('join', ROOM, id, myUsername);
+        });
     });
     
     /*
@@ -78,47 +78,47 @@ function start(){
     We attempt to connet to it by stream and chat.
     */
     socket.on('joined', (userId, username) => {
-        console.log("Socket joined room. Trying to connect to stream")
-        connectToNewStream(userId, username)
-        connectToNewChatUser(userId, username)
-    })
+        console.log("Socket joined room. Trying to connect to stream");
+        connectToNewStream(userId, username);
+        connectToNewChatUser(userId, username);
+    });
     
     /*
     The signalling server tells us that a peer has left the domain.
     If we had a connection to it by chat we remove its container.
     */
     socket.on('disconnected', userId => {
-        console.log('Disconnected user with id: ' + userId)
-        var video = document.getElementById(userId)
+        console.log('Disconnected user with id: ' + userId);
+        var video = document.getElementById(userId);
         if(video){
-            videoContainer.removeChild(document.getElementById(userId))
+            videoContainer.removeChild(document.getElementById(userId));
         }
-    })
+    });
     
     /*
     When someone creates a chat connection to us
     */
     myPeer.on('connection', connection => {
-        console.log("New chat connection with user: " + connection.peer)
+        console.log("New chat connection with user: " + connection.peer);
 
-        chatConnections.push(connection)
+        chatConnections.push(connection);
 
         connection.on('data', data => {
-            addChatEntry(data, connection.options.metadata.username, false)
-        })
+            addChatEntry(data, connection.options.metadata.username, false);
+        });
     
         myPeer.on('close', () => {
-            console.log("Removing chat connection")
-            chatConnections.remove(connection)
-        })
-    })
+            console.log("Removing chat connection");
+            chatConnections.remove(connection);
+        });
+    });
 
     /*
     When recieving a call request (video stream)
     */
     myPeer.on('call', call => {
-        console.log("Incomming call from user with id:" + call.peer)
-        call.answer(myStream)
+        console.log("Incomming call from user with id:" + call.peer);
+        call.answer(myStream);
     
         /*
         If a stream with non-empty video and autio tracks is provided
@@ -126,16 +126,16 @@ function start(){
         */
         call.on('stream', incommingStream => {//This event is called for each media channel.
             if(!streamConnections.includes(call.peer)){//Therefore we check if a connection already exists
-                streamConnections.push(call.peer)
-                var newVideo = document.createElement('video')
-                addNewStream(newVideo, incommingStream , call.peer, call.options.metadata.username)
+                streamConnections.push(call.peer);
+                var newVideo = document.createElement('video');
+                addNewStream(newVideo, incommingStream , call.peer, call.options.metadata.username);
             }
-        })
+        });
 
         call.on('close', () => {
-            streamConnections.remove(call.Peer)
-            newVideo.remove()
-        })
+            streamConnections.remove(call.Peer);
+            newVideo.remove();
+        });
     })
 }
 
@@ -146,11 +146,11 @@ function start(){
  * @param {*} username username of the joined peer
  */
 function connectToNewStream(userId, username){
-    console.log('Calling user with id: ' + userId)
+    console.log('Calling user with id: ' + userId);
 
     var call = myPeer.call(userId, myStream, {metadata: {
         username: myUsername
-    }})
+    }});
 
     /*
     This is called the called peer responds with answer() and
@@ -158,16 +158,16 @@ function connectToNewStream(userId, username){
     */
     call.on('stream', incommingStream => {
         if(!streamConnections.includes(userId)){
-            streamConnections.push(userId)
-            var newVideo = document.createElement('video')
-            addNewStream(newVideo, incommingStream , call.peer, username)
+            streamConnections.push(userId);
+            var newVideo = document.createElement('video');
+            addNewStream(newVideo, incommingStream , call.peer, username);
         }
-    })
+    });
 
     call.on('close', () => {
-        streamConnections.remove(userId)
-        newVideo.remove()
-    })
+        streamConnections.remove(userId);
+        newVideo.remove();
+    });
 }
 
 /**
@@ -178,25 +178,25 @@ function connectToNewStream(userId, username){
  * @param {*} initiator if it was sendt by you
  */
 function addChatEntry(message, username, initiator){
-    var chatElement = document.createElement('div')
-    chatElement.classList.add('chat-entry')
+    var chatElement = document.createElement('div');
+    chatElement.classList.add('chat-entry');
 
-    var label = document.createElement('label')
-    label.innerHTML = username + ":"
+    var label = document.createElement('label');
+    label.innerHTML = username + ":";
 
-    var content = document.createElement('p')
-    content.classList.add('chat-entry-content')
-    content.innerHTML = message
+    var content = document.createElement('p');
+    content.classList.add('chat-entry-content');
+    content.innerHTML = message;
 
     if(initiator){
-        label.classList.add('created')
-        content.classList.add('created')
+        label.classList.add('created');
+        content.classList.add('created');
     }
 
-    chatElement.appendChild(label)
-    chatElement.appendChild(content)
+    chatElement.appendChild(label);
+    chatElement.appendChild(content);
 
-    chatBox.appendChild(chatElement)
+    chatBox.appendChild(chatElement);
 }
 
 /**
@@ -207,8 +207,8 @@ function addChatEntry(message, username, initiator){
 function broadcastMessage(message){
     if(chatConnections.length > 0){
         chatConnections.forEach(connection => {
-            connection.send(message)
-        })
+            connection.send(message);
+        });
     }
 }
 
@@ -221,32 +221,32 @@ function broadcastMessage(message){
 function connectToNewChatUser(userId, username){
     var connection = myPeer.connect(userId, {metadata: {
         username: myUsername
-    }})
+    }});
 
     connection.on('open', () => {
-        console.log("New chat connection with user: " + userId)
-        chatConnections.push(connection)
+        console.log("New chat connection with user: " + userId);
+        chatConnections.push(connection);
         connection.on('data', data => {
-            addChatEntry(data, username, false)
-        })
+            addChatEntry(data, username, false);
+        });
 
         myPeer.on('close', () => {
-            console.log("Removing chat connection")
-            chatConnections.remove(connection)
-        })
-    })
+            console.log("Removing chat connection");
+            chatConnections.remove(connection);
+        });
+    });
 }
 
 /**
  * Function for sending what is in the inputfield of the chat
  */
 function sendMsg(){
-    var message = document.getElementById('chat-input')
+    var message = document.getElementById('chat-input');
     if(message.value){
-        broadcastMessage(message.value)
-        addChatEntry(message.value, myUsername, true)
+        broadcastMessage(message.value);
+        addChatEntry(message.value, myUsername, true);
     }
-    message.value = ""
+    message.value = "";
 }
 
 /**
@@ -281,19 +281,19 @@ function addNewStream(video, stream, id, username) {
     */
     container.addEventListener('click', () => {
         if(container.classList.contains('fullscreen')){
-            container.classList.remove('fullscreen')
+            container.classList.remove('fullscreen');
             document.getElementsByTagName('body')[0].style.overflow = 'auto';
         }
         else{
             container.classList.add('fullscreen')
             document.getElementsByTagName('body')[0].style.overflow = 'hidden';
         }
-    })
+    });
 
-    container.appendChild(video)
-    container.appendChild(label)
+    container.appendChild(video);
+    container.appendChild(label);
     
-    videoContainer.appendChild(container)
+    videoContainer.appendChild(container);
 }
 
 
