7c81610d858adecf709c85e3cd217a99dc7653f6
Simon Jensen <simon.jensen.lg@gmail.com>
2021-03-24 20:30:29 +0100

    Removed unwanted file

--- webrtc/redirect.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-
-//Http server for redirecting to https
-var httpServer = express();
-httpServer.get('*', (req, res) => {  
-    res.redirect('https://' + req.headers.host + req.url);
-})
-
-httpServer.listen(80)
--- webrtc/server.js
+++ webrtc/server.js
@@ -48,3 +48,11 @@ io.on('connection', socket => {
 });
 
 httpsServer.listen(443);
+
+//Http server for redirecting to https
+var httpServer = express();
+httpServer.get('*', (req, res) => {  
+    res.redirect('https://' + req.headers.host + req.url);
+})
+
+httpServer.listen(80)
